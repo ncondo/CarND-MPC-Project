70dc86c5fd0b86fc0ee3c7e2bcd9de8c241174d9
Nicholas Condo <nicholas.condo@gmail.com>
2017-06-20 03:03:13 -0400

    feat: Add weight to sequential actuation cost for smoother steering

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -66,7 +66,7 @@ class FG_eval {
 
     // Minimize the value gap between sequential actuations.
     for (t = 0; t < N-2; ++t) {
-      fg[0] += CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 500 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
       fg[0] += CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
@@ -105,8 +105,8 @@ class FG_eval {
       AD<double> delta0 = vars[delta_start + t - 1];
       AD<double> a0 = vars[a_start + t - 1];
 
-      AD<double> f0 = coeffs[0] + coeffs[1] * x0;
-      AD<double> psides0 = CppAD::atan(coeffs[1]);
+      AD<double> f0 = coeffs[0] + coeffs[1]*x0 + coeffs[2]*x0*x0 + coeffs[2]*x0*x0*x0;
+      AD<double> psides0 = CppAD::atan(coeffs[1] + 2*coeffs[2]*x0 + 3*coeffs[3]*x0*x0);
 
       // Constraint value to be 0.
       fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
